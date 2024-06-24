d6792550eea277dc6016bd48494e8429870901b8
kozak <kozak@MacBook-Pro-kozak.local>
2024-06-24 15:06:28 +0300

    rewrote the stylish formatter

--- src/utils/formatters/stylish.js
+++ src/utils/formatters/stylish.js
@@ -1,28 +1,58 @@
 import _ from 'lodash';
 import { statuses } from '../comparator.js';
 
-const stylish = (fields) => _.sortBy(fields, ['key']).reduce((acc, {
-  key, value, status, oldValue,
-}) => {
-  switch (status) {
-    case statuses.added:
-      acc[`+ ${key}`] = value && typeof value === 'object' ? stylish(value) : value;
-      break;
-    case statuses.removed:
-      acc[`- ${key}`] = value && typeof value === 'object' ? stylish(value) : value;
-      break;
-    case statuses.unchanged:
-      acc[`  ${key}`] = value && typeof value === 'object' ? stylish(value) : value;
-      break;
-    case statuses.changed:
-      acc[`- ${key}`] = oldValue && typeof oldValue === 'object' ? stylish(oldValue) : oldValue;
-      acc[`+ ${key}`] = value && typeof value === 'object' ? stylish(value) : value;
-      break;
-    default:
-      break;
+const SIGNS = {
+  [statuses.added]: '+',
+  [statuses.removed]: '-',
+  [statuses.changed]: '+',
+  [statuses.unchanged]: ' ',
+};
+
+const getOpenOffset = (depth, status) => {
+  const sign = SIGNS[status] ?? SIGNS[statuses.unchanged];
+  return `${' '.repeat(depth * 4 - 2)}${sign} `;
+};
+
+const getCloseOffset = (depth) => `${' '.repeat(depth * 4)}`;
+
+const inner = ({
+  key, value, oldValue, status,
+}, depth) => {
+  const newDepth = depth + 1;
+
+  const valueFormatter = (v, offset) => {
+    if (Array.isArray(v)) {
+      const sortedValues = _.sortBy(v, ['key']);
+      const children = sortedValues.map((node) => inner(node, newDepth)).join('');
+      return `{${children}\n${offset}}`;
+    }
+
+    if (v && typeof v === 'object') {
+      const child = inner(v, newDepth);
+      return `{${child}\n${offset}}`;
+    }
+
+    return v;
+  };
+
+  let result = '';
+  const openOffset = getOpenOffset(newDepth, status);
+  const closeOffset = getCloseOffset(newDepth);
+
+  if (oldValue !== undefined) {
+    const newOpenOffset = getOpenOffset(newDepth, statuses.removed);
+    result += `\n${newOpenOffset}${key}: ${valueFormatter(oldValue, closeOffset)}`;
   }
 
-  return acc;
-}, {});
+  result += `\n${openOffset}${key}: ${valueFormatter(value, closeOffset)}`;
+
+  return result;
+};
+
+const stylish = (fields) => {
+  const sorted = _.sortBy(fields, ['key']);
+  const children = sorted.map((node) => inner(node, 0)).join('');
+  return `{${children}\n}`;
+};
 
 export default stylish;
